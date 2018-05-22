f11296a2f43e24c2ebb47e4c7f6c90216c4fcc23
SamuliAP <samuli.anola-pukkila@hotmail.fi>
2018-05-22 17:30:41 +0300

    database connection script

---
+++ database/connect.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DB_URI, {
+  dbName : process.env.DB_NAME,
+  user   : process.env.DB_USER,
+  pass   : process.env.DB_PASS
+})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => console.log(`Mongoose connected at ${process.env.DB_URI}/${process.env.DB_NAME}`))
