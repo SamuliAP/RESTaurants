deac5fcba8b798e644596a3ee4e88483097da763
SamuliAP <samuli.anola-pukkila@hotmail.fi>
2018-06-17 13:50:00 +0300

    Disable authentication on user creation

--- routes/users.js
+++ routes/users.js
@@ -14,36 +14,54 @@ const {
 router.use('/api/users/:id' , xssMiddleware.sanitizeURI)
 router.use('/api/users/:id' , noSqlMiddleware.sanitizeURI)
 
-// authenticate every users route
-router.use('/api/users'     , authMiddleware.authenticate)
-
 // ------------------------------------
 
 // GET all users
-router.get('/api/users', usersController.getUsers)
+router.get('/api/users', 
+  authMiddleware.authenticate, 
+  usersController.getUsers
+)
 
 // GET user by id
-router.get('/api/users/:id', usersController.getUser)
+router.get('/api/users/:id', 
+  authMiddleware.authenticate, 
+  usersController.getUser
+)
 
 // POST a new user
 router.post('/api/users', usersController.createUser)
 
 // ONLY ADMIN
 // PUT (update) a user completely
-router.put('/api/users/:id', usersController.updateUser)
+router.put('/api/users/:id', 
+  authMiddleware.authenticate, 
+  usersController.updateUser
+)
 
 // PATCH (update) a users email
-router.patch('/api/users/:id/email',usersController.updateUserEmail)
+router.patch('/api/users/:id/email', 
+  authMiddleware.authenticate, 
+  usersController.updateUserEmail
+)
 
 // PATCH (update) a users password
-router.patch('/api/users/:id/password',usersController.updateUserPassword)
+router.patch('/api/users/:id/password', 
+  authMiddleware.authenticate, 
+  usersController.updateUserPassword
+)
 
 // ONLY ADMIN
 // PATCH (update) a users role
-router.patch('/api/users/:id/role',usersController.updateUserRole)
+router.patch('/api/users/:id/role', 
+  authMiddleware.authenticate, 
+  usersController.updateUserRole
+)
 
 // DELETE a user
-router.delete('/api/users/:id',usersController.deleteUser)
+router.delete('/api/users/:id', 
+  authMiddleware.authenticate, 
+  usersController.deleteUser
+)
 
 // ------------------------------------
 
