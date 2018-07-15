d8b4502f9724e5db6e0fb7bb2bd9f5b0cd88a068
SamuliAP <samuli.anola-pukkila@hotmail.fi>
2018-07-15 16:21:01 +0300

    removed data-wrapper from success responses

--- controllers/apiControllers/responses/success.js
+++ controllers/apiControllers/responses/success.js
@@ -1,9 +1,7 @@
 // much of this seems pointless, but this is done like this for loose couplings
 // which leads to much easier future development
 
-successParent = successObject => ({
-  data: successObject
-})
+successParent = successObject => successObject
 
 ok      = successParent
 created = successParent
