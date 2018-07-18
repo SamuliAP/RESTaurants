2fc6e34b65f4131357f8cdd535d5b493e4db06b9
SamuliAP <SamuliAP>
2018-07-18 22:22:15 +0300

    remove redux devtools

--- client/src/store/store/index.js
+++ client/src/store/store/index.js
@@ -5,7 +5,6 @@ import sagaMiddleware, { runSagas } from '../sagas'
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // TODO TÄMÄ POIS
   applyMiddleware(sagaMiddleware)
 )
 
